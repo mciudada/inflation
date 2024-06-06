bcfa78520e901d3928a0e2424e8fe18a97b99933
Elie Wolfe <elie@causarum.org>
2024-06-06 16:28:26 -0400

    Bumping up the classical inflation level to (2,3,3). I have not completed the calculation, but preliminary iteration suggest no incompatibility detected from the bipartite marginals alone.

--- Marias stuff/monogamy check.py
+++ Marias stuff/monogamy check.py
@@ -10,16 +10,14 @@ triangle_classical = InflationProblem(dag={"rho_AB": ["A", "B"],
                                     classical_sources="all",
                                     outcomes_per_party=(2, 2, 2),
                                     settings_per_party=(1, 1, 1),
-                                    inflation_level_per_source=(2, 2, 3),
+                                    inflation_level_per_source=(2, 3, 3),
                                     order=["A", "B", "C"])
 InfLP = InflationLP(triangle_classical, include_all_outcomes=False, verbose=2)
-for m in InfLP.knowable_atoms:
-    if m.n_operators <= 2:
-        print("Known: ", m)
 values = {m.name: m.compute_marginal(PR_box_in_triangle)
           for m in InfLP.knowable_atoms if (m.n_operators <= 2)}
-InfLP.update_values(values, use_lpi_constraints=True)
+InfLP.set_values(values, use_lpi_constraints=True)
 for k, v in InfLP.known_moments.items():
-    print(f"{k}={v}")
+    if k.is_atomic:
+        print(f"{k}={v}")
 InfLP.solve(solve_dual=False)
 print(InfLP.objective_value)
